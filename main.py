9b17409182ef14686f540679a9168019a78e53b5
mikeybow <micbowman25@gmail.com>
2021-09-27 15:06:38 -0500

    finished project

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Generator")
+
+city = input("What's name of the city you grew up in?\n")
+pet = input('What is the name of your pet?\n')
+
+print("Your band name could be\n" + city + ' ' + pet)
